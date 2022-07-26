532ac360a1cc344f7dab205a794a3e6a923a453c
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-07-27 05:56:19 +0700

    Add files via upload

---
+++ CH5.js
@@ -0,0 +1,16 @@
+function stringManipulation(word) {
+
+let text = 'aiueo'
+let vokal = text.slice(0,1)
+let addition = 'nyo';
+//let konsonan =! vokal
+
+if (word.startsWith(vokal)) {
+    return word;
+} else {
+    return word.substr(1,4) + (word.slice(0,1)) + (addition);
+}
+}
+
+console.log(stringManipulation('ayam'));
+console.log(stringManipulation('bebek'));
