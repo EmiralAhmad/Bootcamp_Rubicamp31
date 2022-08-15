b121bb7fb618e1cbafcc403dda64086423bfc38b
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-08-16 00:00:34 +0700

    Update query.sql

--- CH14 University.db/query.sql
+++ CH14 University.db/query.sql
@@ -40,11 +40,10 @@ CREATE TABLE Mata_Kuliah (
     Id_MataKuliah varchar(15) not null primary key,
     Nama varchar(15) not null,
     SKS int(5),
-    Nim varchar(15) not null, foreign key (Nim) references Mahasiswa(Nim)
 );
 
 INSERT INTO Mata_Kuliah 
-VALUES ('MK01','Ushul Fiqih',3,'1201A'),('MK02','Aqidah',3,'1201B'),('MK03','Fiqih',3,'1201C');
+VALUES ('MK01','Ushul Fiqih',3),('MK02','Aqidah',3),('MK03','Fiqih',3);
 
 CREATE TABLE Penilaian (
     Nilai varchar (5) not null,
