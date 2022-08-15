fd92bd4ffad437458a2a3cd44a422e8ac237dece
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-08-15 23:01:25 +0700

    Delete kelas.sql

--- CH14 University.db/kelas.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Kelas (
-    Nim varchar (15) not null,
-    Nip varchar(15) not null, 
-    foreign key (Nip) references Dosen(Nip)
-    foreign key (Nim) references Mahasiswa(Nim)
-);
-
-INSERT INTO Kelas
-VALUES ('1201A','X001'),('1201B','X002'),('1201C','X003'),('1201D','X003');
