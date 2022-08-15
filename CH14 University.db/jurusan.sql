c9bfdd42b8278d1cf0c14c321fc931397f1e1ec5
EmiralAhmad <emiralbahari12@gmail.com>
2022-08-15 10:30:32 +0700

    Your Message

--- CH14 University.db/jurusan.sql
+++ CH14 University.db/jurusan.sql
@@ -1,4 +1,7 @@
-CREATE TABLE Jurusan (
+CREATE TABLE Jurusan ( 
     ID_Jurusan varchar(15) not null primary key,
     Nama varchar(20) not null
 );
+
+INSERT INTO Jurusan
+VALUES ('12011','Akuntansi'),('12012','Manajemen'),('12013','Ekonomi'),('12014','Bisnis Digital');
--- CH14 University.db/mahasiswa.sql
+++ CH14 University.db/mahasiswa.sql
@@ -2,8 +2,9 @@ CREATE TABLE Mahasiswa (
     Nim varchar(15) not null primary key,
     Nama varchar(30) not null,
     Jurusan varchar(15),
-    Alamat text
+    Alamat text,
+    ID_Jurusan varchar (15) not null, foreign key (ID_Jurusan) references Jurusan(ID_Jurusan)
 );
 
 INSERT INTO Mahasiswa 
-VALUES ('1201A','Emiral Ahmad','Akuntansi','Joglo, Cianjur Jawa Barat'),('1201B','Kahfi','Manajemen','Tanah Baru, Bogor Jawa Barat'),('1201C','Fachri Naufal','Ekonomi','Condet Jakarta Timur'),('1201D','Muhaedy','Bisnis Digital','Cinambo, Bandung Jawa Barat');
+VALUES ('1201A','Emiral Ahmad','Akuntansi','Joglo, Cianjur Jawa Barat','12011'),('1201B','Kahfi','Manajemen','Tanah Baru, Bogor Jawa Barat','12012'),('1201C','Fachri Naufal','Ekonomi','Condet Jakarta Timur','12013'),('1201D','Muhaedy','Bisnis Digital','Cinambo, Bandung Jawa Barat','12014');
