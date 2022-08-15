0b9bc5eb5900942dda1e1d19e02f7c2c3f951544
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-08-16 05:35:36 +0700

    Update query.sql

--- CH14 University.db/query.sql
+++ CH14 University.db/query.sql
@@ -1,11 +1,10 @@
 CREATE TABLE Dosen (
     Nip varchar(15) not null primary key,
-    Nama varchar(30) not null,
-    Id_MataKuliah varchar(15) not null, foreign key (Id_MataKuliah) references Mata_Kuliah(Id_MataKuliah)
+    Nama varchar(30) not null
 );
 
 INSERT INTO Dosen
-VALUES ('X001','Dudung Jamaludin','MK01'),('X002','Abdul Aziz','MK02'),('X003','Muhammad Tazakka','MK03');
+VALUES ('X001','Dudung Jamaludin'),('X002','Abdul Aziz'),('X003','Muhammad Tazakka');
 
 CREATE TABLE Jurusan ( 
     ID_Jurusan varchar(15) not null primary key,
