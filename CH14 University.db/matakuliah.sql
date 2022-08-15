96df51fe41b717a55716c26cbb7b49c1fdd50316
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-08-15 22:52:47 +0700

    Delete matakuliah.sql

--- CH14 University.db/matakuliah.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Mata_Kuliah (
-    Id_MataKuliah varchar(15) not null primary key,
-    Nama varchar(15) not null,
-    SKS int(5),
-    Nip varchar(15) not null, foreign key(Nip) references Dosen(Nip)
-);
-
-INSERT INTO Mata_Kuliah 
-VALUES ('MK01','Ushul Fiqih',3,'X001'),('MK02','Aqidah',3,'X002'),('MK03','Fiqih',3,'X003');
