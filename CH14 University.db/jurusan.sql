35c8f7d884d22d31d6539b250bab0a6337b053e0
EmiralAhmad <88173883+EmiralAhmad@users.noreply.github.com>
2022-08-15 23:01:34 +0700

    Delete jurusan.sql

--- CH14 University.db/jurusan.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Jurusan ( 
-    ID_Jurusan varchar(15) not null primary key,
-    Nama varchar(20) not null
-);
-
-INSERT INTO Jurusan
-VALUES ('12011','Akuntansi'),('12012','Manajemen'),('12013','Ekonomi'),('12014','Bisnis Digital');
